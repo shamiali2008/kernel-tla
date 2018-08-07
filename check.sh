464c3a9c0f7bbfa260ac16287f1f347cd71492d0
Catalin Marinas <catalin.marinas@arm.com>
2018-08-07 15:40:45 +0100

    check.sh: Add sed script to extract proc_vars from vars
    This is needed to be able to add a custom Next step which intercepts the
    execution flow by modifying 'pc' and 'stack'.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- check.sh
+++ check.sh
@@ -13,8 +13,13 @@ alias tla2tex="java tla2tex.TLA"
 SPEC=$1
 shift
 
+# Insert ProcessEnabled() for each lable, if found in $SPEC.tla
 grep -q -e "--algorithm" $SPEC.tla && pcal -nocfg $SPEC.tla | tee $SPEC.log
 if grep -q -e "^\s*ProcessEnabled(self)\s*==" $SPEC.tla; then
 	sed -i -e 's%pc\[self\] = ".*"$%& /\\\ ProcessEnabled(self)%' $SPEC.tla
 fi
+
+# Split << pc, stack >> out of the default vars and generate proc_vars
+sed -i -e "/^vars\s*==/,/>>/{s/\<vars\>/proc_vars/;s/\<pc,\s*\|\<stack,\s*//g;s/>>/>>\n\nvars == << proc_vars, pc, stack >>/}" $SPEC.tla
+
 tlc -workers $(nproc) -cleanup $@ $SPEC.tla | tee -a $SPEC.log
