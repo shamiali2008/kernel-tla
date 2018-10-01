3a1aacf90e6a9d9404940a6ab0a00cd7eba7371f
Catalin Marinas <catalin.marinas@arm.com>
2018-10-01 17:10:15 +0100

    check.sh: Remove the tlc -cleanup option
    This doesn't seem to work in the latest TLA+ tools if the states
    directory is not present.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- check.sh
+++ check.sh
@@ -22,4 +22,4 @@ fi
 # Split << pc, stack >> out of the default vars and generate proc_vars
 sed -i -e "/^vars\s*==/,/>>/{s/\<vars\>/proc_vars/;s/\<pc,\s*\|\<stack,\s*//g;s/>>/>>\n\nvars == << proc_vars, pc, stack >>/}" $SPEC.tla
 
-tlc -workers $(nproc) -cleanup $@ $SPEC.tla | tee -a $SPEC.log
+tlc -workers $(nproc) $@ $SPEC.tla | tee -a $SPEC.log
