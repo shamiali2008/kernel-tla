fc4503b81314924b7abaf291ca41fc4027786491
Catalin Marinas <catalin.marinas@arm.com>
2019-01-18 14:01:12 +0000

    check.sh: Fix typo
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- check.sh
+++ check.sh
@@ -13,7 +13,7 @@ alias tla2tex="java tla2tex.TLA"
 SPEC=$1
 shift
 
-# Insert ProcessEnabled() for each lable, if found in $SPEC.tla
+# Insert ProcessEnabled() for each label, if found in $SPEC.tla
 grep -q -e "--algorithm" $SPEC.tla && pcal -nocfg $SPEC.tla | tee $SPEC.log
 if grep -q -e "^\s*ProcessEnabled(self)\s*==" $SPEC.tla; then
 	sed -i -e 's%pc\[self\] = ".*"$%& /\\\ ProcessEnabled(self)%' $SPEC.tla
